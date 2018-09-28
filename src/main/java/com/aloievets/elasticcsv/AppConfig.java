8360044a64d902befffb87391cd2a59adad501fe
andreiloyevets <aloievets@conductor.com>
2018-09-28 17:32:01 +0300

    added auth

--- src/main/java/com/aloievets/elasticcsv/AppConfig.java
+++ src/main/java/com/aloievets/elasticcsv/AppConfig.java
@@ -1,8 +1,15 @@
 package com.aloievets.elasticcsv;
 
+import java.util.function.BiConsumer;
+
 import com.univocity.parsers.csv.CsvParser;
 import com.univocity.parsers.csv.CsvParserSettings;
+
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.bulk.BulkProcessor;
 import org.elasticsearch.action.bulk.BulkProcessor.Listener;
@@ -15,16 +22,29 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.function.BiConsumer;
-
 @Configuration
 @ComponentScan
 public class AppConfig {
 
+    @Bean
+    public CredentialsProvider credentialsProvider() {
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+
+        credentialsProvider.setCredentials(AuthScope.ANY,
+                new UsernamePasswordCredentials("username", "password"));
+
+        return credentialsProvider;
+    }
+
     @Bean(destroyMethod = "close")
-    public RestHighLevelClient elasticClient() {
+    public RestHighLevelClient elasticClient(CredentialsProvider credentialsProvider) {
+
         return new RestHighLevelClient(
-                RestClient.builder(new HttpHost("localhost", 9200, "http")));
+                RestClient.builder(
+                        new HttpHost("host", 9200, "https"))
+                        .setHttpClientConfigCallback(
+                                httpAsyncClientBuilder -> httpAsyncClientBuilder
+                                        .setDefaultCredentialsProvider(credentialsProvider)));
     }
 
     @Bean
@@ -49,7 +69,7 @@ public class AppConfig {
 
     @Bean
     public BulkProcessor bulkProcessor(BiConsumer<BulkRequest, ActionListener<BulkResponse>> bulkConsumer,
-                                          BulkProcessor.Listener bulkListener) {
+            BulkProcessor.Listener bulkListener) {
         return BulkProcessor.builder(bulkConsumer, bulkListener).setConcurrentRequests(10).build();
     }
 
